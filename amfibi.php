32b92c670bc0f45802a867c4d044d2c11ee6ba58
Athaya Dzakiyya <91381116+athayadzakiyya@users.noreply.github.com>
2022-05-05 02:42:40 +0700

    Create amfibi.php

---
+++ amfibi.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel = "stylesheet" href = "style.css"> 
+    <title>AMFIBI</title>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">AMFIBI</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="#">Tambah Data</a>
+              <a class="nav-link" href="#">Bantuan</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div class="background">
+      <img src="" alt="gambar amfibi">
+      <h1>PENGERTIAN</h1>
+      Amfibi atau amfibia (Amphibia) merupakan kelas anggota dari Filum Chordata, 
+      hewan bertulang belakang (vertebrata) hidup di air dan di daratan. Amfibi bertelur di air, 
+      atau menyimpan telurnya di tempat yang lembap dan basah. Ketika menetas, larvanya yang hidup di air atau 
+      tempat basah tersebut dan bernapas dengan insang. Setelah beberapa lama, larva tersebut kemudian berubah bentuk 
+      (metamorfosis) menjadi hewan dewasa, yang umumnya hidup di daratan atau di tempat-tempat yang lebih kering dan 
+      bernapas dengan paru-paru.
+
+      Hewan berlendir dan berdarah dingin ini 
+      dibagi dalam tiga ordo: Anura (katak), Caudata (salamander), Gymnophiona (Apoda).
+
+      <h1>CONTOH</h1>
+      <h3>1. Kodok (Buso sp)</h3>
+      <img scr="" alt="gambar kodok">
+      Tentu kamu sudah tidak asing lagi dengan kodok yang satu ini kan? Hewan yang memiliki nama latin Buso sp ini adalah 
+      salah satu jenis hewan amfibi ini bisa kamu jumpai di sawah, got, sungai, rawa, dan tempat lainnya. Kodok 
+      memiliki kuku dan selaput pada kakinya yang digunakan untuk berenang saat hidup di dalam air. Kaki kodok yang panjang membuatnya memiliki kemampuan untuk melompat dengan jarak yang jauh.
+      Hewan amfibi ini memiliki warna yang gelap, seperti hitam dan kecokelatan serta hidup di permukaan tanah. 
+      Itulah mengapa kodok disebut sebagai hewan amfibi. Tak hanya itu, kodok memiliki lendir yang bercaun 
+      pada bagian kulitnya yang kasar.
+
+      <h3>2. Katak sawah (Rana sp)</h3>
+      <img scr="" alt="gambar katak">
+      Jenis katak yang satu ini memiliki ukuran tubuh yang lebih kecil dibanding kodok. Hewan amfibi ini sering ditemukan 
+      di sawah yang tergenang air. Katak memiliki kaki dan berselaput yang membuatnya lihai berenang.Kulit katak sangat 
+      licin dan memiliki warna yang tidak mencolok. Meski katak memiliki kulit yang licin, namun lendir yang dihasilkan tidak beracun seperti kodok.Katak bernafas 
+      dengan insang saat masih menjadi kecebong, namun saat dewasa ia menggunakan sistem pernafasan invertebrate. Makanan kesukaan katak adalah serangga kecil yang ada di tepian sawah.
+
+      <h3>3. Katak Pohon (Hyla sp)</h3>
+      <img scr="" alt="gambar katak">
+      Katak pohon memiliki ukuran tubuh yang lebih kecil dari katak sawah dan kodok. Warna dari katak pohon ini sangat mencolok dan memiliki lendir yang beracun. Hewan amfibi ini menggunakan juluran lidahnya untuk memakan serangga kecil.Kaki katak telah dilengkapi dengan bantalan yang bisa membuatnya melompat sejauh mungkin dari satu pohon ke pohon lainnya. Katak pohon merupakan contoh hewan yang memiliki metamorfosis sempurna.
+
+      <h3>4. Salamander (Salamander sp)</h3>
+      <img scr="" alt="gambar katak">
+      Salamander dikenal dengan bentuk tubuhnya yang mirip seperti cicak. Salamander bisa kamu temui di habitatnya yang lembab, seperti rawa. Sama halnya dengan cicak, salamander akan mematahkan sebagian tubuhnya jika merasa terancam.Jenis hewan amfibi yang satu ini termasuk ke dalam anggota kelompok hewan ovovivipar, dimana hewan yang dapat melahirkan dan juga menyusui. Salamander juga lebih sering menghabiskan waktunya di dalam air.
+
+      <h3>5. Sesilia/Apoda (Caecilia sp)</h3>
+      <img scr="" alt="gambar katak">
+      Jenis hewan yang satu ini termasuk ke dalam golongan hewan amfibi yang langka. Sesilia memiliki bentuk yang panjang seperti ular, hanya saja ukurannya lebih kecil dan menyukai tempat yang lembab.Tak heran karena bentuknya yang mirip, banyak yang menganggap hewan sesilia adalah jenis cacing atau anak ular.
+
+      <h3>6. Axolotl (Ambystoma mexicanum)</h3>
+      <img scr="" alt="gambar katak">
+      Contoh hewan amfibi selanjutnya adalah axoloti, dimana hewan amfibi yang satu ini banyak menghabiskan waktunya berada di dalam air. Bentuknya yang mirip tak jarang membuat Axolotl disebut sebagai salamander Meksiko.Banyak pecinta binatang yang menjadikannya sebagai hewan peliharaan, karena hewan ini berwarna putih dan memiliki bentuk yang imut dan lucu.
+
+      <h3>7. Diplocaulus</h3>
+      <img scr="" alt="gambar katak">
+      Diplocaulus merupakan salah satu hewan amfibi yang ada pada zaman purba. Menurut para ilmuwan, jenis hewan ini termasuk kelompok hewan pemakan ikan.Dilansir dari ARK Fandom, Diplocaulus menyerupai kadal air besar dengan tubuh rendah dan lembek, kulit berlendir, dan ekor seperti dayung untuk bergerak melalui air rawa yang tergenang.
+
+      <h3>8. Common Mudpuppy (Necturus maculosus)</h3>
+      <img scr="" alt="gambar katak">
+      Hewan ini menjadi salah satu jenis hewan yang berada di bagian timur laut Amerika Serikat. Tak hanya itu, hewan endemik ini juga banyak ditemui di wilayah Kanada dan menyukai tempat yang lembab.Common Mudpuppy lebih suka bersembunyi di dalam air dengan kedalaman 90 kaki. Makanan dari hewan amfibi ini adalah serangga, telur ikan, dan lainnya yang membuat ia mampu bertahan hidup hingga 20 tahun.
+
+      <h3>9. Olm (Proteus anguinus)</h3>
+      <img scr="" alt="gambar katak">
+      Olm dengan nama latin Proteus anguinus berasal dari wilayah Eropa Selatan dan banyak ditemui di perairan tanah berkapus.Hewan amfibi ini memiliki warna kulit yang khas, yaitu berwarna merah muda sama halnya seperti kulit manusia di kawasan Eropa.
+
+      <h3>10. Purple Frog (Nasikabatrachus sahyadrensis)</h3>
+      <img scr="" alt="gambar katak">
+      Purple frog merupakan hewan asal India dengan warna kulit yang cenderung berwarna ungu. Katak jenis ini dikenal sebagai hewan yang suka bersuara sama halnya seperti ayam.Hewan amfibi ini sering menghabiskan waktunya dengan berdiam di dalam tanah dan akan muncul ke permukaan saat musim kawin saja.Nah itulah beberapa contoh hewan amfibi yang kamu perlu ketahui. Hewan amfibi juga bisa menjadi salah satu hewan yang bisa kamu pelihara lho. Kamu harus teliti memilihnya, karena beberapa jenis hewan amfibi memiliki lendir yang beracun.
+</div>
+</body>
+</html>
