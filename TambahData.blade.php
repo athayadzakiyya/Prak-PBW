9217faabccddf992af4f131bb99d2b6636d1793c
Athaya Dzakiyya <91381116+athayadzakiyya@users.noreply.github.com>
2022-05-05 16:54:11 +0700

    Add files via upload

---
+++ TambahData.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel = "stylesheet" href = "style.css"> 
+    <title>Tampilan Data</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="amfibi.blade.php">AMFIBI</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link" href="amfibi.blade.php">Home</a>
+              <a class="nav-linkactive" aria-current="page" href="TambahData.blade.php">Tambah Data</a>
+              <a class="nav-link" href="TampilanData.blade.php">Tampilan Data</a>
+              <a class="nav-link" href="Bantuan.blade.php">Bantuan</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    
+</body>
+</html>
